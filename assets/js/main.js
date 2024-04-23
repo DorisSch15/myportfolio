77b8eaa1daeb0e550b6eade4a126badcbb7dc9b7
Doris <sayitloud15@gmail.com>
2024-04-23 09:00:56 +0200

    changes main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,4 @@
-// // nav/ham toggle
-
-// let navToggle = document.querySelector('.nav__header-ham');
-// let nav = document.querySelector('.links');
-
-// navToggle.addEventListener('click', () => {
-
-//     nav.classList.toggle('links--activ');
-// });
-
-// // toggle / Schrift ahref schwarz/weiss
-
-// let current_url = document.location;
-// document.querySelectorAll(".links__item-a").forEach(function(e){
-//    if(e.href == current_url){
-//       e.classList += " links__item-a--activ";
-//    };
-// });
-
-// testing js
+// navigationHamburger
 
     const btnHamIcon = document.getElementById('btn-hamIcon');
     const mobileMenu = document.getElementById('navMobile');
